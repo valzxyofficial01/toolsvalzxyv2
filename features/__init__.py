3bf36c64b6337ee1c2984d3fad224e5e41adcf3f
ValzxyOfficial <zyaldz77779@gmail.com>
2026-08-18 06:53:03 +0700

    Delete features directory

--- features/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from .email_fake import *
-from .html_deploy import *
-from .ip_lookup import *
-from .wa_banned import *
-from .telegram_report import *
-from .extra_tools import *
